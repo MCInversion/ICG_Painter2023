665aafed296e50c3efd0b02074078fd1b6b5bbda
Martin Čavarga <cavarga1@cr.fmph.uniba.sk>
2023-04-13 09:26:22 +0200

    Filter out more issues

--- ICGPaint.py
+++ ICGPaint.py
@@ -15,44 +15,46 @@ global brush_icon_img
 
 class MainApplication(tk.Frame):
 
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
+	def __init__(self, parent, *args, **kwargs):
+		tk.Frame.__init__(self, parent, *args, **kwargs)
 
-        self.parent = parent
-        self.parent.geometry("{}x{}".format(default_img_width, default_img_height))
+		self.parent = parent
+		self.parent.geometry("{}x{}".format(default_img_width, default_img_height))
 
-        self.toolbar_frame = tk.Frame(self.parent)
-        self.toolbar_frame.pack(side = "top", fill = "x")
-        
-        self.canvas = Canvas(self.parent, bg = default_img_color, height = default_img_height, width = default_img_width)
-        self.canvas.place(x = 0, y = default_toolbar_height)
+		self.canvas = Canvas(self.parent, bg = default_img_color, height = default_img_height, width = default_img_width)
+		self.canvas.place(x = 0, y = default_toolbar_height)
 
-        self.image_project = ImageProject(self.canvas)
-        self.image_project.new()
+		self.toolbar_frame = tk.Frame(self.parent)
+		self.toolbar_frame.pack(side="top", fill="x")
 
-        brush_icon_img = PhotoImage(file=r"res\Brush.png")
-        self.brush = PaintBrush(self.canvas, default_brush_color, default_brush_width)
-        self.brush_button = add_tool_button(self.toolbar_frame, brush_icon_img, command = ActivateToolCommand(self.brush))
-        #self.brush_button.bind('<ButtonPress-1>', self.brush.change_width)
+		brush_icon_img = PhotoImage(file=r"res\Brush.png")
+		self.brush = PaintBrush(self.canvas, default_brush_color, default_brush_width)
+		self.brush_button = add_tool_button(self.toolbar_frame, self.brush, brush_icon_img)
+		
+		self.image_project = ImageProject(self.canvas)
+		self.image_project.new()
 
-        self.menubar = Menu(self.parent)
-        self.filemenu = CommandMenu(self.parent, tearoff = False)
-        self.filemenu.add_command_with_binding("New", NewImageCommand(self.image_project), "Ctrl+N")
-        self.filemenu.add_command_with_binding("Open", OpenFileCommand(filetypes = [("PNG Image files", "*.png;")], image_project = self.image_project), "Ctrl+O")
-        self.filemenu.add_command_with_binding("Save", SaveFileCommand(self.image_project), "Ctrl+S")
-        self.filemenu.add_separator()
-        self.filemenu.add_command_with_binding("Exit", QuitCommand(self), "Ctrl+Q")
-        self.menubar.add_cascade(label="File", menu=self.filemenu)        
-        self.parent.config(menu = self.filemenu)
-        #self.canvas.bind('<ButtonPress-1>', self.brush.draw_pixel)
-        #self.canvas.bind('<B1-Motion>', self.brush.draw_pixel)
-        #self.parent.bind('<ButtonPress-2>', self.brush.change_color)
+		#self.brush_button.bind('<ButtonPress-1>', self.brush.change_width)
+
+		self.menubar = Menu(self.parent)
+		self.filemenu = CommandMenu(self.parent, tearoff=False)
+		self.filemenu.add_command_with_binding(label="New", command=NewImageCommand(self.image_project), accelerator='<Control-N>')
+		self.filemenu.add_command_with_binding(label="Open", command=OpenFileCommand(filetypes = [("PNG Image files", "*.png;")], image_project = self.image_project), accelerator='<Control-O>')
+		self.filemenu.add_command_with_binding(label="Save", command=SaveFileCommand(self.image_project), accelerator='<Control-S>')
+		self.filemenu.add_separator()
+		self.filemenu.add_command_with_binding(label="Exit", command=QuitCommand(self), accelerator='<Control-Q>')
+		self.menubar.add_cascade(label="File", menu=self.filemenu)
+		self.parent.config(menu=self.menubar)
+		
+		#self.canvas.bind('<ButtonPress-1>', self.brush.draw_pixel)
+		#self.canvas.bind('<B1-Motion>', self.brush.draw_pixel)
+		#self.parent.bind('<ButtonPress-2>', self.brush.change_color)
 
 if __name__ == "__main__":
-    root = tk.Tk()
+	root = tk.Tk()
 
-    root.iconbitmap("res/ICGIcon.ico")
-    root.title("Painter")
+	root.iconbitmap("res/ICGIcon.ico")
+	root.title("Painter")
 
-    MainApplication(root).pack(side = "top", fill = "both", expand = True)
-    root.mainloop()
+	MainApplication(root).pack(side = "top", fill = "both", expand = True)
+	root.mainloop()
--- src/bindings.py
+++ src/bindings.py
@@ -5,27 +5,29 @@
 from tkinter import *
 
 class CommandMenu(Menu):
-    def __init__(self, parent, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-        
-    def add_command_with_binding(self, label, command, accelerator = None):
-        self.add_command(label = label, command = command.execute())
-        if accelerator is not None:
-            self.bind_all(accelerator, lambda: command.execute())
+	def __init__(self, parent, *args, **kwargs):
+		super().__init__(parent, *args, **kwargs)
+		self.parent = parent
+		
+	def add_command_with_binding(self, label, command, accelerator = None):
+		self.add_command(label = label, command = command.execute)
+		if accelerator is not None:
+			self.parent.bind(accelerator, command.execute)
 
 
-def add_tool_button(toolbar_frame, tool_class, **button_kwargs):
-    button = Button(master = toolbar_frame, **button_kwargs)
-    
-    def activate_tool():
-        tool_class.activate()
-        button.configure(relief = 'sunken')
-    
-    def deactivate_tool():
-        tool_class.deactivate()
-        button.configure(relief = 'raised')
-    
-    button.configure(command = activate_tool)
-    tool_class.deactivate = deactivate_tool
-    
-    button.pack(side='left')
+def add_tool_button(toolbar_frame, tool, tool_icon_img, **button_kwargs):
+	button = Button(master = toolbar_frame, image = tool_icon_img, **button_kwargs)
+	button.image = tool_icon_img
+
+	def activate_tool():
+		tool.activate()
+		button.configure(relief = 'sunken')
+	
+	def deactivate_tool():
+		tool.deactivate()
+		button.configure(relief = 'raised')
+	
+	button.configure(command = activate_tool)
+	tool.deactivate = deactivate_tool
+	
+	button.pack(side='left')
